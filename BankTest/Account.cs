cb79f6ec0f830c0f53432e9052db8aaa0a1cb545
Sasank Tummalapalli <tummalapallisasank@gmail.com>
2020-01-19 20:36:38 -0800

    First Commit

---
+++ BankTest/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTest
+{
+    class Account
+    {
+        public int AccountNumber { get; set; }
+        public int Balance { get; private set; }
+        public String AccountType { get; set; }
+        public String email { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
---
+++ BankTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //we are creating a variable
+            var myAct = new Account();
+            myAct.AccountNumber = 5582397;
+            myAct.AccountType = "Checking";
+
+        }
+    }
+}
